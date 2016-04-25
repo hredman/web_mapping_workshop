b821d7ad1a402d9e3c84f659f3650eb7ef08b5cc
hredman <harry.redman@districtiii.org>
2016-04-25 10:01:08 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -34,5 +34,8 @@ featureLayer.on('ready', function(){
                 })
 
 
-
-    
+featureLayer.on('ready', function(){
+  this.eachLayer(function(layer){
+    layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
+  });
+});
