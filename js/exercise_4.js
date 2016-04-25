90692fdd1d7e98625175091d45454b087b33bdb4
hredman <harry.redman@districtiii.org>
2016-04-25 10:53:05 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -79,7 +79,7 @@ map.on('locationfound', function(e) {
             coordinates: [e.latlng.lng, e.latlng.lat]
         },
         properties: {
-            'title': 'Here I am!',
+            'title': 'YOU FOUND ME!',
             'marker-color': '#ff8888',
             'marker-symbol': 'star'
         }
