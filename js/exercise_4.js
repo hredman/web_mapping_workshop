123ceef163365b673e822581cc5f194686e0d01d
hredman <harry.redman@districtiii.org>
2016-04-25 11:14:04 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -81,7 +81,7 @@ map.on('locationfound', function(e) {
         },
         properties: {
             'title': 'YOU ARE HERE!',
-            'marker-color': 'RED',
+            'marker-color': '#ff888',
             'marker-symbol': 'star'
         }
     })
