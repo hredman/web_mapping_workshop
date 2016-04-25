f0d2b918fa742236d842a66e2c954c9e4731f27b
hredman <harry.redman@districtiii.org>
2016-04-25 10:51:23 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -69,3 +69,22 @@ map.on('click',function(){
 })
 
 
+var myLocation = L.mapbox.featureLayer().addTo(map);
+map.on('locationfound', function(e) {
+
+    myLocation.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+            'title': 'Here I am!',
+            'marker-color': '#ff8888',
+            'marker-symbol': 'star'
+        }
+    })
+
+})
+
+map.locate({setView:true});
